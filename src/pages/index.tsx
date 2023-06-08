6d167c1092f7e5e3437c35a71eef3d7c8769529c
riko23128 <imai.riko.22@shizuoka.ac.jp>
2023-06-08 12:02:44 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
-        <p>Hello, world!</p>
+        <p>やっほー😊</p>
       </main>
     </>
   );
